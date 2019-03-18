b1a9cf9ba8a9530c9b536bf4570232e6a648e0a3
victoria517 <victoria.517.ramirez@gmail.com>
2019-03-18 08:49:40 -0500

    added controllers

---
+++ controllers/AreaController.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+
+// Defining methods for the HouseController
+module.exports = {
+  findAll: function(req, res) {
+    db.Area.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Area.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Area.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Area.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Area.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
