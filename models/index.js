325e76e9c751b8872843f665c3581fb21d62f4f2
victoria517 <victoria.517.ramirez@gmail.com>
2019-03-18 21:33:29 -0500

    fixed duplicate line

--- models/index.js
+++ models/index.js
@@ -1,10 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-  House: require("./House")
-=======
   House: require("./house"),
   Member: require("./member"),
   Area: require("./area"),
   Task: require("./task")
->>>>>>> f6e87fe99e045ee8d7516089b8d8879e96bf68ec
 };
