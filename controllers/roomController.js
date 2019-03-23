6a340a8279516fbfa24425a28c308d8afb5e8d0d
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-03-22 22:11:48 -0500

    fixed roomController to mySQL syntax

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -3,40 +3,48 @@ const db = require("../models");
 // Defining methods for the HouseController
 module.exports = {
   findAll: function(req, res) {
-    db.Room.find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
+    db.Room.findAll()
+      .then(res => res.json(res))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
     db.Room.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
+      .then(res => res.json(res))
       .catch(err => res.status(422).json(err));
   },
-  findTasksByRoomId: function(req, res) {
-    db.Room.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Task]
-    }).then(function(room) {
-      res.json(room.tasks);
-    });
-  },
+  // findTasksByRoomId: function(req, res) {
+  //   db.Room.findOne({
+  //     where: {
+  //       id: req.params.id
+  //     },
+  //     include: [db.Task]
+  //   }).then(function(room) {
+  //     res.json(room.tasks);
+  //   });
+  // },
   create: function(req, res) {
-    db.Room.create(req.body)
-      .then(dbModel => res.json(dbModel))
+    db.Room.create({name: req.body.name})
+      .then(res => res.json(res))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Room.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
+    db.Room.update(
+      {
+      name: req.params.name
+      },
+      {
+      where: {
+        id: req.params.id
+      }})
+      .then(res => res.json(res))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Room.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
+    db.Room.destroy({
+      where: {
+        id: req.params.id
+      }})
+      .then(res => res.json(res))
       .catch(err => res.status(422).json(err));
   }
 };
