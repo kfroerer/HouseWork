429a73828b4a964af984ba813e0dda7cbbf6bbee
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-04-01 19:06:38 -0500

    fixed cookies

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 export default {
   authUser: () => {
+
     return axios.post("/auth")
   },
   // Gets all rooms
   getRooms: function() {
-    let token = this.getCookie('token');
+    let token = Cookie.get('token');
     return axios.get("/api/rooms",
       {headers: 
         {"Authorization":  `Bearer ${token}`}
@@ -15,19 +16,37 @@ export default {
   },
   // Gets the room with the given id
   getRoom: function(id) {
-    return axios.get("/api/rooms/" + id);
+    let token = Cookie.get('token');
+
+    return axios.get("/api/rooms/" + id,
+      {headers: 
+        {"Authorization":  `Bearer ${token}`}
+      }  
+    );
   },
   // Deletes the room with the given id
   deleteRoom: function(id) {
-    return axios.delete("/api/rooms/" + id);
+    let token = Cookie.get('token');
+
+    return axios.delete("/api/rooms/" + id,
+      {headers: 
+        {"Authorization":  `Bearer ${token}`}
+      }  
+    );
   },
   // Saves a book to the database
   saveRoom: function(roomData) {
-    return axios.post("/api/rooms", roomData);
+    let token = Cookie.get('token');
+
+    return axios.post("/api/rooms", roomData,
+      {headers: 
+        {"Authorization":  `Bearer ${token}`}
+      }  
+    );
   },
   // Gets all tasks for a specific room ID
   getTasksByRoom: function(roomID) {
-    let token = this.getCookie('token');
+    let token = Cookie.get('token');
     return axios.get("/api/tasks/" + roomID + "/room",
       {headers: 
         {"Authorization":  `Bearer ${token}`}
@@ -36,15 +55,27 @@ export default {
   },
   // Gets the task with the given id
   getTask: function(id) {
-    return axios.get("/api/tasks/" + id);
+    let token = Cookie.get('token');
+
+    return axios.get("/api/tasks/" + id,
+      {headers: 
+        {"Authorization":  `Bearer ${token}`}
+      }  
+    );
   },
   // Deletes the task with the given id
   deleteTask: function(id) {
-    return axios.delete("/api/tasks/" + id);
+    let token = Cookie.get('token');
+
+    return axios.delete("/api/tasks/" + id,
+      {headers: 
+        {"Authorization":  `Bearer ${token}`}
+      }  
+    );
   },
   // Saves a task to the database
   saveTask: function(taskData) {
-    let token = this.getCookie('token');
+    let token = Cookie.get('token');
     console.log(taskData);
     return axios.post("/api/tasks", taskData,
       {headers: 
@@ -54,7 +85,7 @@ export default {
   },
   // Updates a task by id to the database
   updateTask: function(id, taskData) {
-    let token = this.getCookie('token');
+    let token = Cookie.get('token');
     console.log(taskData)
     console.log(id)
     return axios.put("/api/tasks/" + id, taskData,
@@ -63,9 +94,13 @@ export default {
       }  
     );
   },
-  getCookie:function(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
-  }
+ 
 };
+
+/**
+ * ### PSEUDOCODE
+ */
+
+//  authUser.then((data) => {
+//    cookie.set('token', data.token);
+//  })
