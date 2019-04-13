f205ec3ff1e6639631c2c7315e459012c026479c
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-04-12 21:49:41 -0500

    updated

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -13,7 +13,7 @@ module.exports = {
       .findOne(
         {
           where: {
-            id: req.params.id
+            taskId: req.params.id
           }
         })
       .then((data) => res.json(data))
@@ -64,7 +64,7 @@ module.exports = {
         },
         {
           where: {
-            id: req.params.id
+            taskId: req.params.id
           }
         }
       )
@@ -75,7 +75,7 @@ module.exports = {
     db.Task
       .destroy({
         where: {
-          id: req.params.id
+          taskId: req.params.id
         }
       })
       .then((data) => res.json(data))
