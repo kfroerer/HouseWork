0441541e3cac5889e63191a0d7712f13ab6461f6
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-04-15 23:34:57 -0500

    fixed layout

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,58 +3,30 @@ import { Button, Form, FormGroup, FormControl, FormLabel, ButtonToolbar } from "
 import { Link } from 'react-router-dom';
 import { Row, Slide, Slider, Caption} from 'react-materialize';
 import { Container } from "../components/Grid";
-import bathroom from '../assets/images/bathroom.jpg'
 
 export default class Landing extends Component {
     constructor(props) {
       super(props);
-  
-      this.state = {
-        email: "",
-        password: "",
-        loginModalShow: false,
-        registerModalShow: false,
-        authenticated: null
-      };
-
-      // this.routeChange = this.routeChange.bind(this);
-
     }
-
-    validateForm () {
-        return this.state.email.length > 0 && this.state.password.length > 0;
-    }
-
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-        });
-    };
     
     onClickRegister = event => {
       event.preventDefault();
       this.props.history.push("/register");
-      //render register page
-      
-
     }
 
-    // routeLogin () {
-    //   let path = '/login';
-    //   this.props.history.push(path);
-    // }
+    onClickLogin = event => {
+      event.preventDefault();
+      this.props.history.push("/login");
+    }
 
-    // routeRegister () {
-    //   let path = '/register';
-    //   this.props.history.push(path)
-    // }
     render() {
         return (
-          <Container fluid>
+                   
+          <Row className="carouselContainer">
           <Slider fullscreen={true}>
             <Slide>
               <li>
-                <img src={bathroom}/>
+                <img src={require('../assets/images/house.jpg')}/>
               </li>
               <Caption> 
                 <h1>Welcome To HouseWork</h1>
@@ -62,27 +34,38 @@ export default class Landing extends Component {
               </Caption>
             </Slide>
           </Slider>
-       
-          
-        <div>
-        <Row style={{alignContent: "center"}}>
-        <ButtonToolbar >
-          <Link to="/register">
-          <Button variant="outline-primary" size="lg">
-            Register 
-          </Button>
-          </Link>
-          <Link to="/login">
-          <Button variant="outline-primary" size="lg">
-            Login
-          </Button>
-          </Link>
-        </ButtonToolbar>
-        </Row>
-        </div>
+          <Slider fullscreen={true}>
+            <Slide>
+              <li>
+                <img src={require('../assets/images/kitchen.jpg')}/>
+              </li>
+              <Caption> 
+                <h1>Welcome To HouseWork</h1>
+                <h3> We help you make your house work </h3>
+              </Caption>
+            </Slide>
+          </Slider>
+          </Row>
+
+                    
+        
+        // <Row className="actionButtons">
+        // <ButtonToolbar >
+        //   <Link to="/register">
+        //   <Button variant="outline-primary" size="lg">
+        //     Register 
+        //   </Button>
+        //   </Link>
+        //   <Link to="/login">
+        //   <Button variant="outline-primary" size="lg">
+        //     Login
+        //   </Button>
+        //   </Link>
+        // </ButtonToolbar>
+        // </Row>
+        
         
 
-       </Container>
        
         );
       }
