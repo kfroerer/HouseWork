901471b66f6d03ff54a6efefe5b614c9faae539a
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-04-24 22:09:07 -0500

    changed house controller to add defaultTasks

--- controllers/houseController.js
+++ controllers/houseController.js
@@ -14,32 +14,52 @@ module.exports = {
       .then(res => res.json(res))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  create: async function(req, res) {
     console.log(req.body)
+    await
     db.House
       .create({
           username: req.body.username,
           password: req.body.password,
           email: req.body.email
-          })
-          .then((house) => {
-            db.defaultTask.findAll()
-            .then((defaultTask) => {
-              defaultTask.map((defaultTask) => {
-                house.adddefaultTask(defaultTask,
-                  {
-                    through: {
-                      status: false,
-                      frequency: defaultTask.frequency,
-                      owner: defaultTask.owner,
-                      date: defaultTask.date,
-                    }
-                  }).then(house => {
-                    return res.json({house:house})
-                  })
-              })
-            })
-          })
+          }, {
+            include: [{
+              model: db.defaultTask,
+              as: 'defaultTask',
+              through: {
+                model: db.Task
+              }
+            }]
+          }
+          )
+
+          await Promise.all(res.defaultTask.map((task, index) => 
+          res.addTask(task, {
+            through: {
+              status: false,
+              frequency: defaultTask[index].frequency,
+              owner: defaultTask[index].owner,
+              date: defaultTask[index].date,
+            }
+          })))
+          // .then((house) => {
+          //   db.defaultTask.findAll()
+          //   .then((defaultTask) => {
+          //     defaultTask.map((defaultTask) => {
+          //       house.adddefaultTask(defaultTask,
+          //         {
+          //           through: {
+          //             status: false,
+          //             frequency: defaultTask.frequency,
+          //             owner: defaultTask.owner,
+          //             date: defaultTask.date,
+          //           }
+          //         }).then(house => {
+          //           return res.json({house:house})
+          //         })
+          //     })
+          //   })
+          // })
           //not sure what to return here....//want to reroute to rooms view
 
       .then(data => res.json(data))
--- models/task.js
+++ models/task.js
@@ -1,7 +1,19 @@
 module.exports = function(sequelize, DataTypes) {
     var Task = sequelize.define("Task", {
-      houseId: DataTypes.INTEGER,
-      taskId: DataTypes.INTEGER,  
+      houseId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "House",
+          key: "id"
+        }
+      },
+      taskId: {
+        type: DataTypes.INTEGER,  
+        refereces: {
+          model: "defaultTask",
+          key: "id"
+        }
+      },
       status: {type: DataTypes.BOOLEAN},
       frequency: {type: DataTypes.STRING, allowNull: true},
       owner: {type: DataTypes.STRING, allowNull: false},
